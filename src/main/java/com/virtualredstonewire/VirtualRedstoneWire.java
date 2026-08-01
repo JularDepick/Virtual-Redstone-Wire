924e1df1f9703c9f2a07629fb344823eed595853
JularDepick <1724834368@qq.com>
2026-08-01 16:02:22 +0800

    消除过时API警告：改用1.20.1兼容构造器注入

--- src/main/java/com/virtualredstonewire/VirtualRedstoneWire.java
+++ src/main/java/com/virtualredstonewire/VirtualRedstoneWire.java
@@ -9,9 +9,9 @@ import com.virtualredstonewire.registry.ModItems;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig.Type;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
@@ -25,20 +25,21 @@ public class VirtualRedstoneWire
      * 作者: JularDepick (https://github.com/JularDepick)
      */
 
-    public VirtualRedstoneWire()
+    public VirtualRedstoneWire(FMLJavaModLoadingContext context)
     {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        IEventBus modEventBus = context.getModEventBus();
+        ModContainer modContainer = context.getContainer();
 
         ModItems.ITEMS.register(modEventBus);
         ModBlocks.BLOCKS.register(modEventBus);
 
-        ModLoadingContext.get().registerConfig(Type.SERVER, ServerConfig.SPEC,
-            MOD_ID + "-server.toml");
+        modContainer.addConfig(new ModConfig(ModConfig.Type.SERVER, ServerConfig.SPEC,
+            modContainer, MOD_ID + "-server.toml"));
 
         DistExecutor.unsafeRunWhenOn(Dist.CLIENT,
             () -> () -> {
-                ModLoadingContext.get().registerConfig(Type.CLIENT, ClientConfig.SPEC,
-                    MOD_ID + "-client.toml");
+                modContainer.addConfig(new ModConfig(ModConfig.Type.CLIENT, ClientConfig.SPEC,
+                    modContainer, MOD_ID + "-client.toml"));
                 modEventBus.addListener(ClientSetup::onClientSetup);
             });
 
